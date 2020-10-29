dc57bb86a4cc81ed093b1f6be446203427f80d29
Victor Augusto <vdbernardes@gmail.com>
2020-10-29 15:14:07 -0300

    Atividade 11

---
+++ Atividade 11/index.js
@@ -0,0 +1,9 @@
+function alteraMaiuscula(){
+    var text = document.getElementById("texto")
+    text.value = text.value.toUpperCase();
+}
+
+function alteraMinuscula(){
+    var text = document.getElementById("texto")
+    text.value = text.value.toLowerCase();
+}
